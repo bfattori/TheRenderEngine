e4f9a3766766ce967d521dee7a76d589a636e5ec
Brett Fattori <bfattori@gmail.com>
2011-08-03 11:18:32 -0400

    * A few more lines of documentation

--- tutorials/tutorial13/particle.js
+++ tutorials/tutorial13/particle.js
@@ -12,6 +12,8 @@ R.Engine.define({
  *
  * @param pos {R.math.Point2D} The starting position of the particle.  A
  *            velocity vector will be derived from this position.
+ * @param [ttl=2000] {Number} The Time To Live (lifespan) of the particle
+ * @param [decel=0] {Number} The rate at which the particle will decelerate over time
  */
 var ExplosionParticle = function() {
    return R.particles.AbstractParticle.extend(/** @scope SimpleParticle.prototype */{
@@ -24,10 +26,17 @@ var ExplosionParticle = function() {
          this.base(ttl || 2000);
          this.setPosition(pos.x, pos.y);
 
+         // Randomly select an angle between 0 and 360 degrees
+         // then get a direction vector from the angle
          var a = Math.floor(R.lang.Math2.random() * 360);
          this.vec = R.math.Math2D.getDirectionVector(R.math.Vector2D.ZERO, R.math.Vector2D.UP, a);
+
+         // Choose a random velocity for the particle and multiply the
+         // direction vector by that velocity
          var vel = 1 + (R.lang.Math2.random() * 10);
          this.vec.mul(vel);
+
+         // Store the deceleration velocity of the particle
          this.decel = decel;
          this.invVel = R.math.Vector2D.create(0, 0);
       },
@@ -53,14 +62,20 @@ var ExplosionParticle = function() {
        * @param time {Number} The engine time in milliseconds
        */
       draw: function(renderContext, time) {
+
+         // If the particle has an assigned deceleration, determine the
+         // inverse velocity (braking velocity) and apply it to the
+         // vector of motion
          if (this.decel > 0 && this.vec.len() > 0) {
             this.invVel.set(this.vec).neg();
             this.invVel.mul(this.decel);
             this.vec.add(this.invVel);
          }
 
+         // Add the vector of motion to the particle
          this.getPosition().add(this.vec);
 
+         // Randomize the color of the particle so it appears to flicker
          var colr = "#fff";
          var s = time - this.getBirth();
          var e = this.getTTL() - this.getBirth();
@@ -73,6 +88,7 @@ var ExplosionParticle = function() {
 
          colr = "#" + (colr.toString(16) + colr.toString(16) + colr.toString(16));
 
+         // Render the particle
          renderContext.setFillStyle(colr);
          renderContext.drawPoint(this.getPosition());
       }
@@ -99,6 +115,7 @@ R.Engine.define({
  *
  * @param pos {R.math.Point2D} The starting position of the particle.  A
  *            velocity vector will be derived from this position.
+ * @param [ttl=500] {Number} The Time To Live (lifespan) of the particle
  */
 var FuseParticle = function() {
    return R.particles.AbstractParticle.extend(/** @scope SimpleParticle.prototype */{
@@ -109,6 +126,7 @@ var FuseParticle = function() {
          this.base(ttl || 500);
          this.setPosition(pos);
 
+         // Set the initial vector of motion and velocity of the particle
          var a = Math.floor(R.lang.Math2.random() * 360);
          this.vec = R.math.Math2D.getDirectionVector(R.math.Vector2D.ZERO, R.math.Vector2D.UP, a);
          var vel = 0.3 + R.lang.Math2.random();
@@ -127,6 +145,7 @@ var FuseParticle = function() {
       draw: function(renderContext, time, dt) {
          this.getPosition().add(this.vec);
 
+         // Randomize the color a bit to make the particle shimmer
          var colr,rgba;
          var s = time - this.getBirth();
          var e = this.getTTL() - this.getBirth();
@@ -135,6 +154,7 @@ var FuseParticle = function() {
 
          rgba = "rgb(" + colr + "," + colr + ",0)";
 
+         // Draw the particle
          renderContext.setFillStyle(rgba);
          renderContext.drawPoint(this.getPosition());
       }
